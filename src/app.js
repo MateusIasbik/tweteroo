9af71407095c8d86f378d727479809886e32651c
Mateus <mateusmpi@gmail.com>
2024-07-09 14:58:19 -0300

    fix: defined initial structure

--- src/app.js
+++ src/app.js
@@ -1,20 +1,29 @@
 import express from "express";
 import cors from "cors";
 import { MongoClient } from "mongodb";
-import dotenv from "dotenv"
+import joi from "joi";
+import dotenv from "dotenv";
+
 dotenv.config();
 
 const app = express()
 app.use(express.json());
-app.use(cors)
-const mongoClient = new MongoClient("mongodb://localhost:27017/test");
+app.use(cors());
+
+const mongoClient = new MongoClient(process.env.DATABASE_URL);
 let db;
 
 mongoClient.connect()
-  .then(() => db = mongoClient.db())
+  .then(() => {
+    db = mongoClient.db();
+    console.log("Conectado ao banco de dados");
+  })
   .catch(err => console.log(err.message));
 
 
-  
+
+
+
+
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Rodando na porta ${port}`));
